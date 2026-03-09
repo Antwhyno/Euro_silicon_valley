426353797125df600290d886efac7d4adb2136f0
Antwhyno <lisamonte251@gmail.com>
2026-03-09 08:00:50 +0100

    link

--- euro_silicon_valley/diapo4.py
+++ euro_silicon_valley/diapo4.py
@@ -3,3 +3,5 @@ import streamlit as st
 def show_page():
     # Pour afficher l'image correspondante
     st.image("images/Slide4.jpg", caption=" ", use_container_width=True)
+
+    st.link_button("map", "https://www.siliconvalleymap.org/")
