570ff1ab6663a32bdb19f621edbc2ad34a84bf3e
Antwhyno <lisamonte251@gmail.com>
2026-03-09 07:08:16 +0100

    import images

--- euro_silicon_valley/diapo1.py
+++ euro_silicon_valley/diapo1.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 def show_page():
     st.header("Diapositive 1")
+    
+    # Pour afficher l'image
+    st.image("images/Slide1.jpg", caption=" ", use_container_width=True)
+    
     st.write("Ceci est le contenu de la diapositive 1.")
