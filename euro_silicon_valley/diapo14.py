d01ec2c8329dd5149844ec0cbe847fad2eb4ac5b
Antwhyno <lisamonte251@gmail.com>
2026-03-09 08:21:38 +0100

    av

--- euro_silicon_valley/diapo14.py
+++ euro_silicon_valley/diapo14.py
@@ -3,3 +3,5 @@ import streamlit as st
 def show_page():
     # Pour afficher l'image correspondante
     st.image("images/Slide14.jpg", caption=" ", use_container_width=True)
+
+    st.link_button("graph", "https://startupgenome.com/report/gser2025/introduction")
